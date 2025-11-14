50e86b21cbc36d7fa8640ab4280d1b62f4a67195
Son Hoang <son.t.hoang@tcu.edu>
2025-11-14 16:36:21 -0600

    q19 submit

---
+++ q19.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int main() {
+    int n, i = 1, sum = 0;
+
+    printf("Enter a positive integer n: ");
+    scanf("%d", &n);
+
+    while (i <= n) {
+        sum += i;
+        i++;
+    }
+
+    printf("Sum from 1 to %d is %d\n", n, sum);
+
+    return 0;
+}
+
